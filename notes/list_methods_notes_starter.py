dbc3d3c244c78a9db38bfcad90cf725031fdb5cd
Geoff Schmit <gcschmit@gmail.com>
2018-11-29 20:21:13 -0600

    added list method notes starter

---
+++ notes/list_methods_notes_starter.py
@@ -0,0 +1,46 @@
+# List Methods Notes
+# METHOD - an ability that an object has.
+#   Like strings, lists are objects that have their own special abilities, 
+#     or list methods.
+
+row = ["Ford", "Audi", "BMW", "Lexus", "Mercedes", "Jeep"]
+print (row)
+
+# 1: Append Method
+#   adds an element to the end of a list
+
+
+#append multiple is a trap!
+#row.append("Ford", "Buick")
+#print (row)
+
+# 2: Pop method
+#   removes an element by index value from a list and returns it
+
+
+# 3: Remove method
+#   removes an element from a list
+ 
+# this method removes the first occurance of an element
+ 
+# 4: Sort method
+#   sorts a list in ascending order
+
+
+# 5: Reverse method
+#   reverses the order of a list
+
+
+# 6: Count method
+#   counts the number of occurances of an element
+
+
+# 7: Index method
+#   displays the index value of an element
+
+
+# 8: Insert method
+#   inserts an element at a specific index value
+
+
+input("Press enter to exit.")
