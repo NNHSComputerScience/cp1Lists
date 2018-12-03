d1921d7360de590a6ab951c67465a9ca18e37de7
Geoff Schmit <gcschmit@gmail.com>
2018-12-03 08:00:09 -0600

    added lab 1 starter code

---
+++ labs/semester_grades_shell.py
@@ -0,0 +1,53 @@
+# Chapter 5: Lab 1
+# Semester Grades Program
+
+# ***** Initalize variables and lists *****
+
+
+
+# ***** Get student name and add to student list *****
+
+
+
+# ***** Get student scores for 5 assignments and add to grade list *****
+
+ 
+        
+# ***** Display student scores *****
+
+
+        
+# ***** Delete lowest grade *****
+
+
+
+# ***** Accumulate student's grades to calculate average *****
+
+
+    
+# ***** Add student average to a final_grade list *****
+
+    
+    
+# ***** See if there are additional students *****
+
+
+    
+# ***** Display number of students in class *****
+
+
+
+# ***** Display Alpha listing of student *****
+
+
+
+# ***** Calculate and display class average *****
+
+
+
+# ***** Determine and print breakdown of grades given for class *****
+
+
+
+
+input("Press enter key to exit")
